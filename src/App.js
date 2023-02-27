863abdba67f740c81a73043b2ec685e6f129451a
shivamchawla29june <shivamchawla.29june@gmail.com>
2023-02-26 18:44:39 -0800

    Fixing router build issue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.scss";
-import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import SignUp from "./SignUpContainer";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+// import Button from "@mui/material/Button";
+// import SignUp from "./SignUpContainer";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Listings from "./containers/landlord/listings";
 import ListingInfo from "./containers/landlord/listingInfo";
 import ListingSearch from "./containers/landlord/listingSearch";
 import ApplicationInfo from "./containers/landlord/applicationInfo";
 
 function App() {
-  const [showSignUp, setShowSignUp] = useState(false);
+  // const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <div className="App">
-      {/* <header className="App-header">
+    <BrowserRouter>
+      <div className="App">
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -26,20 +27,21 @@ function App() {
           Learn React
         </a>
       </header> */}
-      {/* <div>Here</div> */}
-      <div>
-        <div>
-          <Button onClick={() => setShowSignUp(true)}>Sign Up</Button>
-        </div>
-        {showSignUp && <SignUp />}
+        {/* <div>Here</div> */}
+        {/* <div>
+          <div>
+            <Button onClick={() => setShowSignUp(!showSignUp)}>Sign Up</Button>
+          </div>
+          {showSignUp && <SignUp />}
+        </div> */}
+        <Routes>
+          <Route exact path="/" element={<Listings />} />
+          <Route path="/listingInfo" element={<ListingInfo />} />
+          <Route path="/listingSearch" element={<ListingSearch />} />
+          <Route path="/application" element={<ApplicationInfo />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route exact path="/" element={<Listings />} />
-        <Route path="/listingInfo" element={<ListingInfo />} />
-        <Route path="/listingSearch" element={<ListingSearch />} />
-        <Route path="/application" element={<ApplicationInfo />} />
-      </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
