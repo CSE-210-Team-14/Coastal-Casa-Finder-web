e90b1fe04ef1f31dd2aa7ac5868c3cf092436b18
Sarat Sreepathy <55508719+ssreepathy@users.noreply.github.com>
2023-02-28 21:04:25 -0800

    Edit listing functionality

---
+++ src/containers/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink as Link } from 'react-router-dom';
+
+const NavBar = () => {
+
+    return (
+        <>
+            <div>
+                <li>
+                <Link to="/"></Link>
+                </li>
+                <li>
+                <Link to="/"></Link>
+                </li>
+                <li>
+                <Link to="/"></Link>
+                </li>
+                <li>
+                <Link to="/"></Link>
+                </li>
+            </div>
+        </>
+    )
+}
--- src/containers/landlord/listingInfo.jsx
+++ src/containers/landlord/listingInfo.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { EditText, EditTextarea } from 'react-edit-text';
 import "./listingInfo.scss";
 import {useLocation} from "react-router-dom";
 
@@ -8,6 +9,19 @@ const ListingInfo = () => {
 
     const currentListing = location.state;
 
+    const [editState, setEditState] = useState(true);
+
+    const handleClick = () => {
+        if (editState == true) {
+            setEditState(false);
+            console.log("success");
+        }
+        else {
+            setEditState(true);
+            console.log("success2");
+        }
+    }
+
     return (
         <>
             <h2 className="listing-name">{currentListing.name}</h2>
@@ -16,29 +30,29 @@ const ListingInfo = () => {
             </div>
             <h3 className="listing-categ">About</h3>
             <div className="desc-box">
-                <p className="categ-text">{currentListing.desc}</p>
+                <EditText readonly={editState} className="categ-text" defaultValue={currentListing.desc} style={{backgroundColor: "#FFF"}}></EditText>
             </div>
             <h3 className="listing-categ">Apartment Amenities</h3>
             <div className="desc-box">
-                <p className="categ-text">{currentListing.amenities}</p>
+                <EditText readonly={editState} className="categ-text" defaultValue={currentListing.amenities} style={{backgroundColor: "#FFF"}}></EditText>
             </div>
             <h3 className="listing-categ">Property Description</h3>
             <div className="desc-box">
                 <div className="property-edit">
                     <p className="property-text">Bedroom: </p>
-                    <p className="property-entry">{currentListing.bedroom}</p>
+                    <EditText readonly={editState} className="property-entry" defaultValue={currentListing.bedroom} style={{backgroundColor: "#FFF"}}></EditText>
                 </div>
                 <div className="property-edit">
                     <p className="property-text">Bathroom: </p>
-                    <p className="property-entry">{currentListing.bathroom}</p>
+                    <EditText readonly={editState} className="property-entry" defaultValue={currentListing.bathroom} style={{backgroundColor: "#FFF"}}></EditText>
                 </div>
                 <div className="property-edit">
                     <p className="property-text">Location: </p>
-                    <p className="property-entry">{currentListing.city}</p>
+                    <EditText readonly={editState} className="property-entry" defaultValue={currentListing.city} style={{backgroundColor: "#FFF"}}></EditText>
                 </div>
             </div>
             <div>
-                <button className="edit-button">Update/Edit</button>
+                <button className="edit-button" onClick={()=>{handleClick()}}>Update/Edit</button>
             </div>
         </>
     );
