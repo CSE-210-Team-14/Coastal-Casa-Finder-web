e9e27d178698c34a184aded12906f7d99eb6e3a5
Jessica Echterhoff <j.m.echt@gmail.com>
2023-03-01 19:16:54 -0800

    layout for search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { SearchIcon, MenuIcon, UserCircleIcon, UserIcon, } from "@heroicons/react/solid";
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import { DateRangePicker } from "react-date-range";
+import { SearchIcon, MenuIcon, UserCircleIcon } from "@heroicons/react/solid";
+import { MdOutlineShower } from 'react-icons/md';
+import { BiBed } from 'react-icons/bi';
+import Slider from '@mui/material/Slider';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import DatePicker from 'react-date-picker/dist/entry.nostyle';
+import "./datepicker.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'react-calendar/dist/Calendar.css';
 
 type HeaderProps = {
   placeholder?: any;
@@ -16,17 +19,17 @@ const Header: React.FunctionComponent<HeaderProps> = ({
   placeholder, showSignup}) => {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const [noOfGuest, setNoOfGuest] = useState<any>(1);
+  const [noBed, setNoBed] = useState<any>(1);
+  const [noBath, setNoBath] = useState<any>(1);
+  const [price, setPrice] = useState<any>(1000);
+  const [dist, setDist] = useState<any>(10);
 
   const handleSelect = (ranges: any) => {
     setStartDate(ranges.selection.startDate);
-    setEndDate(ranges.selection.endDate);
   };
 
   const selectionRange = {
     startDate: startDate,
-    endDate: endDate,
     key: "selection",
   };
 
@@ -40,7 +43,9 @@ const Header: React.FunctionComponent<HeaderProps> = ({
     <header
       className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10" >
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
-        
+        <img
+          src={require('./cc.png')} style={{width: 80, height: 80}}
+        />
       </div>
 
       <div className="flex items-center border-2 rounded-full py-2  md:border-2 md:shadow-sm">
@@ -48,11 +53,11 @@ const Header: React.FunctionComponent<HeaderProps> = ({
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-500 placeholder-gray-400"
-          placeholder={placeholder || "Start your Search"}
+          placeholder={placeholder || "Search for location"}
           type="text"
         />
         <SearchIcon
-          className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full
+          className="hidden md:inline-flex h-8 bg-blue-400 text-white rounded-full
         p-2 cursor-pointer md:mx-2"
         />
       </div>
@@ -63,32 +68,53 @@ const Header: React.FunctionComponent<HeaderProps> = ({
         </div>
       </div>
       {searchInput && (
-        <div className="flex flex-col col-span-3 mx-auto"
-        >
-          <DateRangePicker
-            ranges={[selectionRange]}
-            minDate={new Date()}
-            rangeColors={["#FD5B61"]}
-            onChange={handleSelect}
-          />
-          <div className="flex items-center border-b mb-4">
-            <h2 className="text-2xl flex-grow font-semibold">
-              Number Of Guest
-            </h2>
-            <UserIcon className="h-5" />
+        <div className="flex flex-col col-span-3 mx-auto">
+          <div className="flex items-center mb-6">
+            Start Date
+          <DatePicker onChange={handleSelect} value={startDate} className='react-date-picker'/>
+          </div>
+          <div className="flex items-center mb-4">
+            Radius (miles)
+            <Slider
+            size="small"
+            defaultValue={20}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            onChange={setDist}
+            />
+          </div>
+          
+          <div className="flex items-center mb-6 pl-4">
+            <BiBed className="h-5" />
             <input
-              value={noOfGuest}
-              onChange={(e) => setNoOfGuest(e.target.value)}
+              value={noBed}
+              onChange={(e) => setNoBed(e.target.value)}
               min={1}
               type="number"
-              className="w-12 pl-2 text-lg outline-none"
+              className="w-12 pl-2 text-lg items-center outline-none"
+            />
+            <MdOutlineShower className="h-5" />
+            <input
+              value={noBath}
+              onChange={(e) => setNoBath(e.target.value)}
+              min={1}
+              type="number"
+              className="w-12 pl-2 text-lg outline-none items-center"
+            />
+            <BsCurrencyDollar className="h-5" />
+            <input
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              min={1}
+              type="number"
+              className="w-20 pl-2 text-lg items-center outline-none"
             />
           </div>
           <div className="flex">
             <button onClick={resetInput} className="flex-grow text-gray-500">
               Cancel
             </button>
-            <button onClick={search} className="flex-grow text-red-400">
+            <button onClick={search} className="flex-grow text-blue-400">
               Search
             </button>
           </div>
