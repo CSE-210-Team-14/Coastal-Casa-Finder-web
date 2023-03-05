379c6f58897590d46e6fd3a00c961711bf16580c
shivamchawla29june <shivamchawla.29june@gmail.com>
2023-03-04 23:03:33 -0800

    BUild modules issues

--- src/containers/landlord/listings.jsx
+++ src/containers/landlord/listings.jsx
@@ -1,8 +1,8 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Listing from "./listing";
 import Application from "./application";
 import "./listings.scss";
-import axios from "axios";
 
 const Listings = () => {
   const [fetchDataFromDB, setFetchDataFromDB] = useState(true);
