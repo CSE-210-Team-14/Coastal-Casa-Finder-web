6c2b342bc7ab3b14f6711cc162b57f41b25b6bdc
shivamchawla29june <shivamchawla.29june@gmail.com>
2023-03-04 17:11:45 -0800

    axios error

--- src/containers/landlord/listings.jsx
+++ src/containers/landlord/listings.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Listing from "./listing";
 import Application from "./application";
-import axios from "axios";
 import "./listings.scss";
 
 const Listings = () => {
